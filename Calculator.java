6a5c336813b2d21c0e30636c825c424a3945791e
ethan2110 <132939487+ethan2110@users.noreply.github.com>
2023-05-09 11:33:15 -0400

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,47 +1,50 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*; //Imports the java Stack and Scanner classes needed for the method
+// Stack enables all data to operations at one end only. So the only element that can be removed is the element at the top of the stack, and only one item can be read or removed at a given time
+//also using stack so I can do pemdas operations more easily, stack allows me to manipulate the order of the array more easily than having to use an array list as thinga re added from the top and removed from the bottom
 
 public class Calculator {
     
-    private static final String OPERATORS = "+-*/^";
+    private static final String OPERATORS = "+-*/^"; // Final string meaning that the string cannot be changed also needs to be capitalized so it functions
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //Initializes the scanner
 
-        System.out.println("Welcome to the calculator app!");
-        System.out.println("Please enter your equation:");
+        System.out.println("Enter your equation:");
 
-        String equation = scanner.nextLine();
-        double result = evaluate(equation);
+        String equation = sc.nextLine(); //scanner to get input from next line
+        double result = evaluate(equation); //evaluates the equaiton typed into the scanner
 
         System.out.println("Result: " + result);
     }
 
     private static double evaluate(String equation) {
-        Stack<Double> operands = new Stack<>();
-        Stack<Character> operators = new Stack<>();
-        StringBuilder sb = new StringBuilder();
+        Stack<Double> operands = new Stack<>(); //operands initialized as a new stack  
+        Stack<Character> operators = new Stack<>(); //operators initialized as a new stack
+        StringBuilder sb = new StringBuilder(); //initializes sb as a string builder becauses its faster and uses less memomory
 
         for (int i = 0; i < equation.length(); i++) {
-            char c = equation.charAt(i);
+            char c = equation.charAt(i); //character c is set to be the value of the array equation at the point i
 
-            if (Character.isDigit(c) || c == '.') {
-                sb.append(c);
-            } else if (OPERATORS.indexOf(c) != -1) {
+            if (Character.isDigit(c) || c == '.') { //Checks if the digit in c is equal to a period 
+                sb.append(c); //appends the string at array value c
+            } 
+            else if (OPERATORS.indexOf(c) != -1) { //Gets the index of c in the string OPERATORS
                 double operand = Double.parseDouble(sb.toString());
-                sb.setLength(0);
+                sb.setLength(0); //sets lenght of sb to 0
 
-                while (!operators.empty() && precedence(c, operators.peek())) {
-                    double b = operands.pop();
-                    double a = operands.pop();
+                while (!operators.empty() && pemdas(c, operators.peek())) { // returns the value of the top ("front") of the collection without removing the element from the collection
+                    double b = operands.pop(); //removes elements that do not follow rules of pemdas
+                    double a = operands.pop(); //sets the doubles b and a to the popped elements of operands
                     operands.push(applyOperator(a, b, operators.pop()));
                 }
 
-                operators.push(c);
-                operands.push(operand);
-            } else if (c == '(') {
-                operators.push(c);
-            } else if (c == ')') {
+                operators.push(c); //pushes c to the op of the stack
+                operands.push(operand); //pushes value of operand to the top of the stack
+            } 
+            else if (c == '(') {
+                operators.push(c); //pushes element c to the front of the stack
+            } 
+            else if (c == ')') { //[pushes elemet c to the front of the stack of operations because it is an operation inside of parenthesis
                 double operand = Double.parseDouble(sb.toString());
                 sb.setLength(0);
 
@@ -67,35 +70,38 @@ public class Calculator {
             operands.push(applyOperator(a, b, operators.pop()));
         }
 
-        return operands.pop();
+        return operands.pop(); //returns operands 
     }
 
-    private static boolean precedence(char op1, char op2) {
-        if (op2 == '(' || op2 == ')') {
+    private static boolean pemdas(char op1, char op2) {// PEMDAS operation order
+        if (op2 == '(' || op2 == ')') {//Parenthesis come first
             return false;
-        } else if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) {
+        } 
+        else if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) { //multiplication and division valued second and addition and subtraciton valued third
             return false;
-        } else if (op1 == '^' && (op2 == '*' || op2 == '/' || op2 == '+' || op2 == '-')) {
+        } 
+        else if (op1 == '^' && (op2 == '*' || op2 == '/' || op2 == '+' || op2 == '-')) { //exponent pemdas
             return false;
-        } else {
+        } 
+        else {
             return true;
         }
     }
 
     private static double applyOperator(double a, double b, char op) {
         switch (op) {
-            case '+':
+            case '+': // if + detected then add b to a
                 return a + b;
-            case '-':
+            case '-': // if + detected then subtract b to a
                 return a - b;
-            case '*':
+            case '*': // if + detected then multiply a by b
                 return a * b;
-            case '/':
+            case '/': // if + detected then divide a by b
                 return a / b;
-            case '^':
+            case '^': // if ^ detected then a is raised to the power of b
                 return Math.pow(a, b);
-            default:
-                throw new IllegalArgumentException("Invalid operator: " + op);
+            default: // if an illegal operator is detected then throw an illegal argument exception
+                throw new IllegalArgumentException("Invalid operator: " + op); // illegal argument exception being thrown indicates an illegal argument inside of the code
         }
     }
 }
